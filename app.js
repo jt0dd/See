57ac38483dadae1f0c6485a8ab81a9b2438eed0d
Jonathan Todd <42309086+enviziion@users.noreply.github.com>
2019-03-17 14:42:58 -0500

    fix

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
 import DataSet from "./classes/data-set.js";
 console.log("Predator activated...");
-new DataSet("letter-mini", 150, data => {
+new DataSet("letter", 150, data => {
   console.log("Data loaded", data);
   const config = {
-    inputSize: 100,
+    inputSize: 625,
     //inputRange: 20,
     //hiddenLayers: [20, 20],
     outputSize: 1,
     learningRate: 0.01,
     decayRate: 0.999
   };
-  const net = new brain.NeuralNetworkGPU(config);
+  const net = new brain.NeuralNetwork(config);
   let trainingSet = [];
-  for (var i = 0; i < 1000; i++) {
-    data.t.forEach(input => {
-      trainingSet.push({
-        input: input,
-        output: [1]
-      });
+  data.t.forEach(input => {
+    trainingSet.push({
+      input: input,
+      output: [1]
     });
-    data.f.forEach(input => {
-      trainingSet.push({
-        input: input,
-        output: [0]
-      });
+  });
+  data.f.forEach(input => {
+    trainingSet.push({
+      input: input,
+      output: [0]
     });
-  }
+  });
   console.log('Built training set', trainingSet);
   net.train(trainingSet);
   data.t.forEach(input => {
     let result = Math.round(net.run(input));
-    console.log('Should output [1] ->[' + result[0] + ']');
+    console.log('Should output [1] ->[' + result   + ']');
   });
   data.f.forEach(input => {
     let result = Math.round(net.run(input));
-    console.log('Should output [0] ->[' + result[0] + ']');
+    console.log('Should output [0] ->[' + result + ']');
   });
 });
