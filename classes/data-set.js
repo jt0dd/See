3c8b42af413f7b37c1d7b37ee1d973536c447669
Jonathan Todd <42309086+enviziion@users.noreply.github.com>
2019-03-17 22:19:32 -0500

    Success

--- classes/data-set.js
+++ classes/data-set.js
@@ -1,6 +1,12 @@
 function simplify(data, threshold) {
   let result = [];
-  data.forEach(val => {
+  let resultsRaw = [];
+  for (let i = 0; i < data.length; i += 4) {
+    let total = data[i] + data[i + 1] + data[i + 2];
+    let average = total / 3;
+    resultsRaw.push(average);
+  }
+  resultsRaw.forEach(val => {
     if (val > threshold) {
       result.push(1);
     } else {
