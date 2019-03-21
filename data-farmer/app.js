2f75a76ce974a025d3f52398a4942afb5d437413
Jonathan Todd <42309086+enviziion@users.noreply.github.com>
2019-03-21 09:57:57 -0500

    Progress

--- data-farmer/app.js
+++ data-farmer/app.js
@@ -1,1 +1,15 @@
+import Loader from "./classes/loader.js";
+import DataGrabber from './classes/data-grabber.js';
 
+document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
+    let dataGrabber = new DataGrabber();
+  }
+};
+
+let loader = new Loader(24, 25, data => {
+  console.log("data", data);
+  data.forEach(bitmaps => {
+
+  });
+});
---
+++ data-farmer/classes/data-grabber.js
@@ -0,0 +1,45 @@
+function handleMouseMove(event) {
+  var eventDoc, doc, body;
+
+  event = event || window.event; // IE-ism
+
+  // If pageX/Y aren't available and clientX/Y are,
+  // calculate pageX/Y - logic taken from jQuery.
+  // (This is to support old IE)
+  if (event.pageX == null && event.clientX != null) {
+    eventDoc = (event.target && event.target.ownerDocument) || document;
+    doc = eventDoc.documentElement;
+    body = eventDoc.body;
+
+    event.pageX =
+      event.clientX +
+      ((doc && doc.scrollLeft) || (body && body.scrollLeft) || 0) -
+      ((doc && doc.clientLeft) || (body && body.clientLeft) || 0);
+    event.pageY =
+      event.clientY +
+      ((doc && doc.scrollTop) || (body && body.scrollTop) || 0) -
+      ((doc && doc.clientTop) || (body && body.clientTop) || 0);
+  }
+  this.mouseX = event.pageX;
+  this.mouseY = event.pageY;
+  this.box.style.left = event.pageX - 12 + 'px';
+  this.box.style.top = event.pageY - 12 + 'px';
+  //console.log('x, y', event.pageX, event.pageY);
+
+}
+
+class DataGrabber {
+  constructor() {
+    this.box = document.getElementById("box");
+    console.log("box", this.box);
+    this.mouseX;
+    this.mouseY;
+    document.onmousemove = ()=>{
+      handleMouseMove.apply(this);
+    }
+
+  }
+  track(canvas) {}
+}
+
+export default DataGrabber;
